5454fc71eeb9e4e3874debbde46dd24fdeed9364
to.maykie <to.maykie@gmail.com>
2018-06-24 18:56:54 -0400

    updated with get requests

--- serverside/app.js
+++ serverside/app.js
@@ -7,7 +7,9 @@ const userSchema = require('./users');
 //instructing the app to use body parser
 app.use(bodyParser.json());
 
+
 //our user model schema
+//lets the app use it
 const User = mongoose.model('User', userSchema);
 
 //connecting the app to mongoose
@@ -16,19 +18,34 @@ mongoose.connect ('mongodb://maykie:cheese123@ds261540.mlab.com:61540/meowster')
 const db = mongoose.connection;
 
 //gets the homepage for you
-app.get('/', function(req, res){
-  res.send('Welcome to the Meowster landing page!');
+app.get('/homepage', function(req, res){
+  res.send('Welcome to the Meowster home page!');
     
 });
 
-//gets the list of users from the database
+//gets the signup page
+app.get('/signup', function(req,res){
+  res.send('new users signup')
+});
+
+//add new user to the database
+app.post('/User',function(req,res){
+  res.send({type:'post'})
+  
+});
 
 
-
+//gets the login page
+app.get('/login',function(req,res){
+  res.send('User login')
+  
+});
 
 
 
 app.get('/', (req, res) => res.send('Alls Good'));
+
+
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
 console.log("Yippee its running");
   
